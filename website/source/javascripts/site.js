ebfbe284530578ebfdcce6c3fae7d296788f9a76
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-07-17 22:37:00 +0900

    website contents (wip)

--- website/source/javascripts/site.js
+++ website/source/javascripts/site.js
@@ -1,1 +1,15 @@
-// This is where it all goes :)
+document.addEventListener("DOMContentLoaded", () => {
+  const now = Date.now();
+  const count = 360;
+  const fragment = document.createDocumentFragment();
+  const container = document.getElementById("images");
+  [...Array(count)].forEach((_, i) => {
+    const elem = document.createElement("img");
+    elem.setAttribute("src", `/images/twemoji-check.png?${now}_${i}`);
+    elem.setAttribute("class", `m-1 bg-gray-200`);
+    elem.setAttribute("width", `30px`);
+    elem.setAttribute("height", `30px`);
+    fragment.appendChild(elem);
+  });
+  container.appendChild(fragment);
+});
